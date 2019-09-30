9651b49dcfa026d8af3c62be968e3f4b0dcf279a
Shcherbakova Darya <shcherbakova.darya@gmail.com>
2019-09-30 16:26:14 -0400

    solved problem 2

--- Problem_2.py
+++ Problem_2.py
@@ -1,1 +1,12 @@
- 
+fib_row = [1, 2]
+even_sum = 2
+print(fib_row[-1])
+
+while fib_row[-1]<=4e6:
+    sum(fib_row[-2:])
+    #print(sum(fib_row[-2:]))
+    fib_row.append(sum(fib_row[-2:]))
+    if fib_row[-1]%2 == 0:
+        even_sum += fib_row[-1]
+print(fib_row)
+print(even_sum)
